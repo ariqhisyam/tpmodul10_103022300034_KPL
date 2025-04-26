7bb9b009eaf0d6bb475b89515ac19d1efce49f18
Ariq Hisyam Nabil <ariqhisyam62@gmail.com>
2025-04-27 00:33:21 +0700

    Add project files.

---
+++ tpmodul10_103022300034/Mahasiswa.cs
@@ -0,0 +1,11 @@
+﻿namespace tpmod10_103022300034
+{
+    using System;
+
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+
+}
